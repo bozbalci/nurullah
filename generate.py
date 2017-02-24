7b75c246b05358868e5a4a5116c2dfb2b3271d67
Berk Ozbalci <berkozbalci@gmail.com>
2017-02-25 02:58:08 +0300

    Fix indentation

--- generate.py
+++ generate.py
@@ -3,5 +3,5 @@ from demo import istiklal
 song = istiklal
 
 for i, n in enumerate(song):
-	i = str(i).zfill(4)
-	print("ffmpeg -y -f lavfi -i \"" + str(n) + "\" " + str(i) + ".wav")
+    i = str(i).zfill(4)
+    print("ffmpeg -y -f lavfi -i \"" + str(n) + "\" " + str(i) + ".wav")
