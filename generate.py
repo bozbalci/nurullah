b176e1d51d0cc8047fdcde0a638748a259c63a7d
Berk Ozbalci <berkozbalci@gmail.com>
2017-02-25 17:54:29 +0300

    Remove generate.py

--- generate.py
+++
@@ -1,9 +0,0 @@
-from demo import istiklal
-
-song = istiklal
-
-for track in range(len(istiklal)):
-	print("mkdir -p /tmp/nurullah/" + str(track))
-	for i, n in enumerate(song[track]):
-		i = str(i).zfill(4)
-		print("ffmpeg -y -f lavfi -i \"" + str(n) + "\" /tmp/nurullah/"+str(track)+"/" + str(i) + ".wav")
